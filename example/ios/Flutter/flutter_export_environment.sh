4618f0dd50643b7fd1d827eb3737e6d1aa15ea56
edz <>
2021-05-27 10:56:42 +0800

    init

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/edz/Desktop/flutterSDK/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/edz/Downloads/plugins-device_info-v0.4.2-7/packages/device_info/device_info/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/edz/Desktop/flutterSDK/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
